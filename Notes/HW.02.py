cbba612bc6bc4cf1eb3bd204619cd4cd187f8bb8
piotrekbog34 <piotrekbog34@gmail.com>
2023-09-14 21:02:00 +0200

    Create HW.02.py

---
+++ Notes/HW.02.py
@@ -0,0 +1,46 @@
+num = float(input("Wprowadź liczbę: "))
+if num >= 0:
+    result = "Liczba jest dodatnia"
+if num < 0:
+    result = "Liczba jest ujemna"
+print(result)
+
+
+num = float(input("Wprowadź liczbę: "))
+if num >= 0:
+    result = "Liczba jest dodatnia"
+else:
+    result = "Liczba jest ujemna"
+print(result)
+
+
+#<	Mniej
+#<=	Mniejsza lub równa
+#>	Więcej
+#>=	Większy lub równy
+#==	Równy
+#!=	Nie równe
+
+
+is_next = True
+num = int(input("Enter the number of points: "))
+if num >= 83:
+    is_next = True
+    result = "True"
+else:
+    is_next = False
+    result = "False"
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
