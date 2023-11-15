5d159c7eed50c6a23f170fcc41fa981b5783f2f8
piotrekbog34 <piotrekbog34@gmail.com>
2023-11-15 20:56:52 +0100

    .

--- Notes/HW.04.py
+++ Notes/HW.04.py
@@ -7,8 +7,7 @@ def amount_payment(payment):
 
     return total_payment
 
-        
-
+#######################################      
 
 def prepare_data(data):
     total_data = 0
@@ -25,8 +24,7 @@ def prepare_data(data):
     data.sort()
     return data
 
-
-
+#######################################
 
 def format_ingredients(items):
     if not items:
@@ -38,7 +36,7 @@ def format_ingredients(items):
         formatted_items = f"{formatted_items} and {items[-1]}"
         return formatted_items
 
-
+#######################################
 
 ects_to_grades = {
     "F": 1,
@@ -66,7 +64,7 @@ def get_grade(key):
 def get_description(key):
     return ects_to_descriptions.get(key, None)
 
-
+#######################################
 
 def lookup_key(dictionary, value):
     keys = []
@@ -75,7 +73,7 @@ def lookup_key(dictionary, value):
             keys.append(key)
     return keys
 
-
+#######################################
 
 def split_list(grade):
     if not grade:
@@ -87,7 +85,7 @@ def split_list(grade):
     
     return (lower_or_equal_avg, greater_than_avg)
 
-
+#######################################
 
 points = {
     (0, 1): 2,
@@ -118,7 +116,7 @@ def calculate_distance(coordinates):
 
     return total_distance
 
-
+#######################################
 
 terra = [[1, 1, 5, 10], [10, 2], [1, 1, 1]]
 power = 1
@@ -132,6 +130,7 @@ def game(terra, power):
                 break
     return power
 
+#######################################
 
 def is_valid_pin_codes(pin_list):
     if not pin_list:
@@ -150,5 +149,114 @@ def is_valid_pin_codes(pin_list):
 
     return len(unique_pins) == len(pin_list)
 
+#######################################
+
+from random import randint
+
+def get_random_password():
+    password_length = 8
+    password_characters = "()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+    random_password = [password_characters[randint(0, len(password_characters)-1)] for _ in range(password_length)]
+
+    return ''.join(random_password)
+
+random_password = get_random_password()
+print(random_password)
+
+#######################################
+
+def is_valid_password(password):
+    if len(password) != 8:
+        return False
+
+    has_upper = False
+    has_lower = False
+    has_digit = False
+
+    for char in password:
+        if char.isupper():
+            has_upper = True
+        elif char.islower():
+            has_lower = True
+        elif char.isdigit():
+            has_digit = True
+
+    return has_upper and has_lower and has_digit
+
+password_to_check = "Abcd1234"
+result = is_valid_password(password_to_check)
+
+if result:
+    print("Hasło jest silne i bezpieczne")
+else:
+    print("Hasło nie spełnia wymagań bezpieczeństwa")
 
 
+#######################################
+
+from random import randint
+
+def get_random_password():
+    result = ""
+    count = 0
+    while count < 8:
+        random_symbol = chr(randint(40, 126))
+        result = result + random_symbol
+        count = count + 1
+    return result
+
+
+def is_valid_password(password):
+    if len(password) != 8:
+        return False
+
+    has_upper = False
+    has_lower = False
+    has_num = False
+
+    for ch in password:
+        if ch.isupper():
+            has_upper = True
+        elif ch.islower():
+            has_lower = True
+        elif ch.isdigit():
+            has_num = True
+
+    return has_upper and has_lower and has_num
+
+def get_password():
+    max_attempts = 100
+    attempts = 0
+
+    while attempts < max_attempts:
+        password_candidate = get_random_password()
+
+        if is_valid_password(password_candidate):
+            return password_candidate
+        else:
+            attempts += 1
+
+    print("Nie udało się wygenerować hasła")
+    return None
+
+#######################################
+
+from pathlib import Path
+
+def parse_folder(path):
+    files = []
+    folders = []
+
+    if path.is_dir():
+        files = [item.name for item in path.iterdir() if item.is_file()]
+        folders = [item.name for item in path.iterdir() if item.is_dir()]
+
+    return files, folders
+
+#######################################
+
+import sys
+
+def parse_args():
+    result = ' '.join(sys.argv[1:])
+    return result
